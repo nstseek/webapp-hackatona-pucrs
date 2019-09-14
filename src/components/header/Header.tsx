490e6eb2e12506ae8b29f7d5e2710837daeb6afe
esquadrao-anti-praga <esquadrao-anti-praga>
2019-09-14 15:07:45 -0300

    Cria header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,54 @@
-import React from "react";
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeartbeat, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { Input } from '@material-ui/core';
+import './Header.css';
 
+interface HeaderState {
+    searchField: string;
+}
 
-export default class Header extends React.Component {
+export default class Header extends React.Component<any, HeaderState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            searchField: ''
+        };
+    }
 
+    render() {
+        return (
+            <nav className='header'>
+                <div className='logo-container'>
+                    <FontAwesomeIcon icon={faHeartbeat} className='icon' />
+                    <span className='title'>Assistencia medica</span>
+                </div>
+                <div className='search-bar-container'>
+                    <Input
+                        className='search-bar'
+                        placeholder='Digite um endereço'
+                        type='text'
+                        onChange={this.setSearchField}
+                        disableUnderline={true}
+                    />
+                    <div onClick={this.search} className="icon-container">
+                        <FontAwesomeIcon
+                            icon={faSearch}
+                            className='icon search-icon'
+                        />
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+
+    setSearchField = (event: any) => {
+        this.setState({
+            searchField: event.target.value
+        });
+    };
+
+    search = () => {
+        console.log('search clicked');
+    };
 }
---
+++ src/components/map/Map.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './Map.css';
+
+export default class Map extends React.Component {
+    render() {
+        return <div />;
+    }
+}
